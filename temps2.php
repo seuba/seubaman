53714bed3e588525035687d0f48dc15f5a01b6f4
seuba <49449750+seuba@users.noreply.github.com>
2019-04-23 15:59:36 +0200

    Update temps2.php

--- temps2.php
+++ temps2.php
@@ -52,8 +52,7 @@ echo '{"foundSignupDate": granizado"}';
 
 ?>
 <script>
-        var http = require('http');
-
+       
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World!');
