456be783c8786d0ce77d2ede23fa2cb9d02a5357
seuba <49449750+seuba@users.noreply.github.com>
2019-04-23 14:53:51 +0200

    Update temps2.php

--- temps2.php
+++ temps2.php
@@ -31,18 +31,7 @@ curl_setopt($ch, CURLOPT_URL,"https://pub.s1.exacttarget.com/ttddvbbzxv4");
 curl_setopt($ch, CURLOPT_POST, TRUE);
 // definimos cada uno de los parámetros
 curl_setopt($ch, CURLOPT_POSTFIELDS, "temps=$temps");
-  curl_setopt($ch, CURLOPT_HTTPHEADER => array(
-    "Accept: */*",
-    "Cache-Control: no-cache",
-    "Connection: keep-alive",
-    "Content-Type: text/plain",
-    "Host: weatheralbert.herokuapp.com",
-    "Postman-Token: 72865532-e6ae-487e-b523-dafe456e25b0,9dd662ab-908d-48a2-bbea-13decc508c6a",
-    "User-Agent: PostmanRuntime/7.11.0",
-    "accept-encoding: gzip, deflate",
-    "cache-control: no-cache",
-    "content-length: 588"
-  )),
+ 
 // recibimos la respuesta y la guardamos en una variable
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $remote_server_output = curl_exec ($ch);
