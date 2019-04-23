ea95bf8e5981a0b9459df77497dcf4fd644755fb
seuba <49449750+seuba@users.noreply.github.com>
2019-04-23 14:08:02 +0200

    Update temps2.php

--- temps2.php
+++ temps2.php
@@ -22,19 +22,16 @@ $json4 = '{
 $object = json_decode($json4, true);
 
 
-	$temps = $object['inArguments'][0]['message'] . '<br>' ;
+	$temps = $object['inArguments'][0]['message'];
 
+$params = $temps;
 
-$curl = curl_init();
 curl_setopt_array($curl, array(
-  CURLOPT_URL => "https://pub.s1.exacttarget.com/ttddvbbzxv4?temps=".$temps,
+  CURLOPT_URL => "https://pub.s1.exacttarget.com/ttddvbbzxv4",
   CURLOPT_POST => true,
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => "",
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 30,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1
+  CURLOPT_POSTFIELDS => $params,
 ));
+$curl = curl_init();
 $response = curl_exec($curl);
 $err = curl_error($curl);
 curl_close($curl);
