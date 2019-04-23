2acf7842595f6a6e5e76f411bd0486e6f7fdc457
seuba <49449750+seuba@users.noreply.github.com>
2019-04-23 13:31:59 +0200

    Update temps2.php

--- temps2.php
+++ temps2.php
@@ -1,6 +1,45 @@
 <?php
-$albert = '{"foundSignupDate": "2016-03-10"}';
+/*$json4 = '{
+        "inArguments":[ 
+                {
+                    "emailAddress": "albert@gmail.com"
+                }]
+            ,
+        "outArguments": [
+                 
+                ],
+        "activityObjectID": "1234abcd-56ef-78gh-90ij-9876klmn5432",
+        "journeyId": "1234abcd-56ef-78gh-90ij-9876klmn5432",
+        "activityId": "1234abcd-56ef-78gh-90ij-9876klmn5432",
+        "definitionInstanceId": "1234abcd-56ef-78gh-90ij-9876klmn5432",
+        "activityInstanceId": "1234abcd-56ef-78gh-90ij-9876klmn5432",
+        "keyValue": "someContactKeyHere",
+        "mode": 0
+}';
+*/
 
-echo $albert;
+$json4 = file_get_contents('php://input');
+$object = json_decode($json4, true);
+
+
+	$email = $object['inArguments'][0]['emailAddress'] . '<br>' ;
+
+
+$curl = curl_init();
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://pub.s1.exacttarget.com/ttddvbbzxv4?email=".$email,
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1
+));
+$response = curl_exec($curl);
+$err = curl_error($curl);
+curl_close($curl);
+if ($err) {
+  echo "cURL Error #:" . $err;
+}
+
 
 ?>
