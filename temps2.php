c81959354eef793751b8c08696c36e7f5b263147
seuba <49449750+seuba@users.noreply.github.com>
2019-04-23 14:16:52 +0200

    Update temps2.php

--- temps2.php
+++ temps2.php
@@ -30,7 +30,7 @@ curl_setopt($ch, CURLOPT_URL,"https://pub.s1.exacttarget.com/ttddvbbzxv4");
 // indicamos el tipo de petición: POST
 curl_setopt($ch, CURLOPT_POST, TRUE);
 // definimos cada uno de los parámetros
-curl_setopt($ch, CURLOPT_POSTFIELDS, "temps='.$temps.'");
+curl_setopt($ch, CURLOPT_POSTFIELDS, "temps=$temps");
  
 // recibimos la respuesta y la guardamos en una variable
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
