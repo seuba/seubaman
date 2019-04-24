b3adb2f88c76e4a861a44041ca39944df2d08237
seuba <49449750+seuba@users.noreply.github.com>
2019-04-24 13:05:18 +0200

    Update temps2.php

--- temps2.php
+++ temps2.php
@@ -1,5 +1,5 @@
 <?php
-/*$json4 = '{
+$json4 = '{
         "inArguments":[ 
                 {
                     "message": "a"
@@ -17,9 +17,9 @@
         "mode": 0
 }';
 
-*/
 
-        $json4 = file_get_contents('php://input'); 
+
+      //  $json4 = file_get_contents('php://input'); 
 $object = json_decode($json4, true);
    $temps = $object['inArguments'][0]['message'];
 
