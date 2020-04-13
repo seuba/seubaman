33acf599261663baf71a532c84154a746a42fad9
seuba <49449750+seuba@users.noreply.github.com>
2020-04-13 21:39:17 +0200

    Update corona.php

--- corona.php
+++ corona.php
@@ -25,3 +25,35 @@ echo ($err2);
 curl_close($curl2);
 
 ?>
+<pre class="seuba" id="id">aaa</pre>
+<script>
+
+function parseHTML(markup) {
+    if (markup.toLowerCase().trim().indexOf('<!doctype') === 0) {
+        var doc = document.implementation.createHTMLDocument("");
+        doc.documentElement.innerHTML = markup;
+        return doc;
+    } else if ('content' in document.createElement('template')) {
+       // Template tag exists!
+       var el = document.createElement('template');
+       el.innerHTML = markup;
+       return el.content;
+    } else {
+       // Template tag doesn't exist!
+       var docfrag = document.createDocumentFragment();
+       var el = document.createElement('body');
+       el.innerHTML = markup;
+       for (i = 0; 0 < el.childNodes.length;) {
+           docfrag.appendChild(el.childNodes[i]);
+       }
+       return docfrag;
+    }
+}
+var htmll = <?php echo $response2 ?>;
+var links2 = parseHTML(htmll).getElementsByTagName('title')[0].text;
+
+var str = links2;
+
+document.getElementById("id").innerHTML= str;
+
+</script>
