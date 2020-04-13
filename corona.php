64ca8caaaa0040509f82659ead291c404ad8dc86
seuba <49449750+seuba@users.noreply.github.com>
2020-04-13 21:57:17 +0200

    Update corona.php

--- corona.php
+++ corona.php
@@ -19,7 +19,7 @@ curl_setopt_array($curl2, array(
 ));
 
 $response2 = curl_exec($curl2);
-echo ("'".$response2."'");
+echo htmlentities($response2);
 $err2 = curl_error($curl2);
 echo ($err2);
 curl_close($curl2);
