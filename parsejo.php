5383c08b2d97113659472ec4e3e63abf2d5e4f79
seuba <49449750+seuba@users.noreply.github.com>
2020-04-20 21:26:39 +0200

    Update parsejo.php

--- parsejo.php
+++ parsejo.php
@@ -12,21 +12,7 @@ foreach ($nodes as $node)
     $tmp_dom->appendChild($tmp_dom->importNode($node,true));
     }
 $innerHTML.=trim($tmp_dom->saveHTML()); 
- $innerHTML =  str_replace('<div class="maincounter-number">
-<span style="color:#aaa">',"",$innerHTML);
-
-$innerHTML =  str_replace('</span>
-</div><div class="maincounter-number">',"",$innerHTML);
-
-    
-   $innerHTML =  str_replace('</span>
-</div><div class="maincounter-number" style="color:#8ACA2B ">
-<span>',"",$innerHTML);
-
- $innerHTML =  str_replace('</span>
-</div>',"",$innerHTML);
-$innerHTML =  str_replace('<span>',"",$innerHTML);
-$innerHTML =  str_replace(',',"",$innerHTML);
+ 
 
 echo $innerHTML;
 ?>
