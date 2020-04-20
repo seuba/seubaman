5818601519825d4729073494e2ddc132009a6e0c
seuba <49449750+seuba@users.noreply.github.com>
2020-04-20 20:38:14 +0200

    Update parsejo.php

--- parsejo.php
+++ parsejo.php
@@ -3,5 +3,15 @@ $doc = new DomDocument();
 $doc->loadHTMLFile('https://www.worldometers.info/coronavirus/country/spain/');
 $thediv = $doc->getElementById('maincounter-wrap');
 echo $thediv->textContent;
+foreach($thediv AS $thedivs)
+    {
+  
+   
+        echo $thedivs -> textContent;
 
+
+   
+
+
+}
 ?>
