f650683f61fb57b082d0371423bf84f55b9e9518
seuba <49449750+seuba@users.noreply.github.com>
2020-04-20 21:09:31 +0200

    Update parsejo.php

--- parsejo.php
+++ parsejo.php
@@ -3,15 +3,17 @@ $doc = new DomDocument();
 $doc->loadHTMLFile('https://www.worldometers.info/coronavirus/country/spain/');
 $thediv = $doc->getElementById('maincounter-wrap');
 echo $thediv->textContent;
-foreach($thediv AS $thedivs)
+
+$dom = new DomDocument();
+$doc->loadHTMLFile('https://www.worldometers.info/coronavirus/country/spain/');
+$classname = 'maincounter-number';
+$finder = new DomXPath($dom);
+$nodes = $finder->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' $classname ')]");
+$tmp_dom = new DOMDocument(); 
+foreach ($nodes as $node) 
     {
-  
-   
-        echo $thedivs -> textContent;
-
-
-   
-
-
-}
+    $tmp_dom->appendChild($tmp_dom->importNode($node,true));
+    }
+$innerHTML.=trim($tmp_dom->saveHTML()); 
+echo $innerHTML;
 ?>
